7039f9ea161ca30e246448ccc68237bdce4b7e10
dykim <dykim4225@gmail.com>
2025-08-01 12:59:24 +0900

    [feature-test] step 8
    Signed-off-by: dykim <dykim4225@gmail.com>

--- test_booking_scheduler.py
+++ test_booking_scheduler.py
@@ -5,7 +5,7 @@ import pytest
 from schedule import Customer, Schedule
 from communication import SmsSender, MailSender
 from booking_scheduler import BookingScheduler
-from test_communication import TestableSmsSender
+from test_communication import TestableSmsSender, TestableMailSender
 
 CAPACITY_PER_HOUR = 3
 UNDER_CAPACITY = 1
@@ -68,8 +68,14 @@ def test_예약완료시_SMS는_무조건_발송(booking_scheduler_with_sms_mock
 
     assert sms_mock.send_called
 
-def test_이메일이_없는_경우에는_이메일_미발송():
-    pass
+def test_이메일이_없는_경우에는_이메일_미발송(booking_scheduler):
+    mail_sender = TestableMailSender()
+    booking_scheduler.set_mail_sender(mail_sender)
+    schedule =  Schedule(ON_THE_HOUR, UNDER_CAPACITY, CUSTOMER)
+
+    booking_scheduler.add_schedule(schedule)
+
+    assert mail_sender.send_mail_count == 0
 
 def test_이메일이_있는_경우에는_이메일_발송():
     pass
